9a5550e21c91bc4b6f857d91a7d5297816a8dc2b
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-11 23:58:52 +0530

    add datatypes and variable

---
+++ A__Datatype and variable/declaration.java
@@ -0,0 +1,7 @@
+class Codex{
+	public static void main(String[] args){
+		int a;
+			System.out.println(a);
+	}
+}hello 
+hello 
---
+++ A__Datatype and variable/reinilitization.java
@@ -0,0 +1,7 @@
+class Codex{
+	public static void main(String[] args){
+		int a = 10;
+		 a = 50 ;
+			System.out.println(a);
+	}
+}
