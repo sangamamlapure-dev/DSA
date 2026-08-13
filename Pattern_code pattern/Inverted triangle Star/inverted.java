309a5167415ebceeca64f7f6913216d86ec9b93c
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-13 23:38:36 +0530

    add inverted triangle

---
+++ Pattern_code pattern/Inverted triangle Star/inverted.java
@@ -0,0 +1,11 @@
+class Codex{
+	public static void main(String[] args){
+		int num=5;
+		for(int i=1;i<=num;i++){
+			for(int j=num;j>=i;j--){
+				System.out.print("*");
+			}
+				System.out.println();
+		}
+	}
+}
