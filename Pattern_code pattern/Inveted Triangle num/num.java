440d44169d4a297554b011fd789b524f271f973b
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-13 23:46:19 +0530

    add reverse num

---
+++ Pattern_code pattern/Inveted Triangle num/num.java
@@ -0,0 +1,11 @@
+class Codex{
+	public static void main(String[] args){
+		int num=5;
+		for(int i=1;i<=num;i++){
+			for(int j=5;j>=i;j--){
+				System.out.print(j);
+			}
+				System.out.println();
+		}
+	}
+}
