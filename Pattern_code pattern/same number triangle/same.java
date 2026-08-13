0f93a2a7ada0fcbb1f880ea2432b883e54a6086c
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-13 23:29:43 +0530

    increse by 1

---
+++ Pattern_code pattern/same number triangle/same.java
@@ -0,0 +1,13 @@
+class Codex{
+	public static void main(String[] args){
+		int num=5;
+		int count=1;
+		for(int i=1;i<=num;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print(count);
+					count++;
+			}
+				System.out.println();
+		}
+	}
+}
