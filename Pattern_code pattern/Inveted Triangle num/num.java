e5fc6f2c6272ba07e76059f95b3d18d62acac4d1
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-13 23:51:16 +0530

    inverted triangle

--- Pattern_code pattern/Inveted Triangle num/num.java
+++ Pattern_code pattern/Inveted Triangle num/num.java
@@ -1,9 +1,9 @@
 class Codex{
 	public static void main(String[] args){
 		int num=5;
-		for(int i=1;i<=num;i++){
-			for(int j=5;j>=i;j--){
-				System.out.print(j);
+		for(int i=num;i>=1;i--){
+			for(int j=1;j<=i;j++){
+				System.out.print(i);
 			}
 				System.out.println();
 		}
