3b9b4dd1e498de8abb2af95c87b4eed2dd6bec02
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-14 23:35:38 +0530

    right aligned

---
+++ Pattern_code pattern/Right aligned inverted/right.java
@@ -0,0 +1,14 @@
+class Codex{
+	public static void main(String[] args){
+		for(int i=1 ;i<=5; i++){
+			for(int j=1; j<=5;j++){
+				if(i==j || j>i){
+					System.out.print("*");
+				}else{
+					System.out.print(" ");
+				}
+			}
+				System.out.println();
+		}
+	}
+}
