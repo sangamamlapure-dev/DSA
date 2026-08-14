74886b082e5d13a566a64b93379f9159dc77315f
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-14 23:36:42 +0530

    add right triangle

---
+++ Pattern_code pattern/Right triangle/right.java
@@ -0,0 +1,13 @@
+class Codex{
+	public static void main(String[] args){
+		int num=1;
+		for(int i=1;i<=num;i++){
+			for(int j=1;j<=num-1;j++){
+				System.out.print(" ")
+			}
+			for(){
+			
+			}
+		}
+	}
+}
