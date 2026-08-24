81d63c357f827610c81b50ac21059b6d4a56129c
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-24 23:44:56 +0530

    add count of num

---
+++ Demo code/Digit exctraction code/count.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 12345;
+        int count = 0;
+
+        while (n > 0) {
+            count++;
+            n = n / 10;
+        }
+
+        System.out.println("Count = " + count);
+    }
+}
