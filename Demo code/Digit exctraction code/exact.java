cb3e97afffd94426bfa36239cedc135a18e74059
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-24 23:36:54 +0530

    print number

---
+++ Demo code/Digit exctraction code/exact.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 12345;
+
+        while (n > 0) {
+            int digit = n % 10;
+            System.out.println(digit);
+            n = n / 10;
+        }
+    }
+}
