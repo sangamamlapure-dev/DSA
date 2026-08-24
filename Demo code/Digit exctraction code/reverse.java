3de298a50adb0c70e637b2d7e2d3d5de87f1c316
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-24 23:43:19 +0530

    add reverse num

---
+++ Demo code/Digit exctraction code/reverse.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 12345;
+        int rev = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+
+        System.out.println("Reverse = " + rev);
+    }
+}
