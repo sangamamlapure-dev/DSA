5784717e7b4f3976de103a0fbe428de50d38de53
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-26 22:51:46 +0530

    largest no

---
+++ Demo code/Arrys/large.java
@@ -0,0 +1,14 @@
+class Array3 {
+    public static void main(String[] args) {
+        int[] arr = {25, 10, 45, 30, 15};
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Largest = " + max);
+    }
+}
