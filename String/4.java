ffb3c0b55295a6bf419ad519d749a0005126bc62
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-08-28 23:59:49 +0530

    add java 4

---
+++ String/4.java
@@ -0,0 +1,11 @@
+class Largest {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 40;
+
+        if (a > b)
+            System.out.println("Largest = " + a);
+        else
+            System.out.println("Largest = " + b);
+    }
+}
