a3c0a099dcd2036b9a57fae1940e8e0f44733415
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-05 23:31:29 +0530

    print duplicate element

---
+++ Arry-practics/Duplicate element/duplicate.java
@@ -0,0 +1,16 @@
+class Codex{
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 1, 3, 2};
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    System.out.print(arr[i] + " ");
+                }
+            }
+        }
+    }
+}
