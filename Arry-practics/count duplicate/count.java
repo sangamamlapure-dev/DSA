7e9010743a46e87d066c48c19d5997169f241c37
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-05 23:35:32 +0530

    count of duplicate num

---
+++ Arry-practics/count duplicate/count.java
@@ -0,0 +1,20 @@
+class Codex{
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 1, 3, 2};
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
