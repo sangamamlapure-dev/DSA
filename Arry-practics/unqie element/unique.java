add6cfc09d137e5024fe7ce6131a3a99206db325
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-05 23:25:40 +0530

    enter unique element

---
+++ Arry-practics/unqie element/unique.java
@@ -0,0 +1,21 @@
+class Codex{
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 1, 3};
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
