a45ac4e84bc1bc6a0b5c200c1a08613f1be2499a
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-05 23:22:20 +0530

    cube of every element

---
+++ Arry-practics/cube every element/cube.java
@@ -0,0 +1,10 @@
+class codex{
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] * arr[i] * arr[i] + " ");
+        }
+    }
+}
