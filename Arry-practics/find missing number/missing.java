98c90a433fb9adbd3b519de48277a5d0ff189e87
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-09 22:52:36 +0530

    add missing number

---
+++ Arry-practics/find missing number/missing.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 4, 5};
+        int n = 5;
+
+        int total = n * (n + 1) / 2;
+        int sum = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            sum = sum + a[i];
+        }
+
+        int missing = total - sum;
+
+        System.out.println(missing);
+    }
+}
