6d1535665dcd2ab1388f39279e9dde43875ef072
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-09 23:05:10 +0530

    product of element

---
+++ Arry-practics/product of element/product.java
@@ -0,0 +1,14 @@
+class Codex {
+    public static void main(String[] args) {
+        int n = 523146;
+        int product = 1;
+
+        while (n > 0) {
+            int digit = n % 10;
+            product = product * digit;
+            n = n / 10;
+        }
+
+        System.out.println("Product = " + product);
+    }
+}
