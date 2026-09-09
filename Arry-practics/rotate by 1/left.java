2731538820a9f3629b7869216064d0a40b34b3f2
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-09 22:55:15 +0530

    add rotate by 1

---
+++ Arry-practics/rotate by 1/left.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4};
+
+        int first = a[0];
+
+        for (int i = 0; i < a.length - 1; i++) {
+            a[i] = a[i + 1];
+        }
+
+        a[a.length - 1] = first;
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
