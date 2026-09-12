1eff59b6672b9962e19d3e8391519d8ee827b0d0
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-12 23:54:17 +0530

    count words

---
+++ String/Practics/p5.java
@@ -0,0 +1,10 @@
+class main {
+    public static void main(String[] args) {
+
+        String s = "Java is very easy";
+
+        String[] words = s.split(" ");
+
+        System.out.println("Words = " + words.length);
+    }
+}
