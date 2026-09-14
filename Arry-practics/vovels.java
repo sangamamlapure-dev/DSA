be20e7ccc891bcb8ab1651cc15c1fa0e0244d48e
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-14 23:57:37 +0530

    add count of vovels

---
+++ Arry-practics/vovels.java
@@ -0,0 +1,23 @@
+class Vowels {
+    public static void main(String[] args) {
+
+        String str = "Sangam Amlapure";
+
+        int count = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if(ch == 'a' || ch == 'e' || ch == 'i' ||
+               ch == 'o' || ch == 'u' ||
+               ch == 'A' || ch == 'E' || ch == 'I' ||
+               ch == 'O' || ch == 'U') {
+
+                count++;
+            }
+        }
+
+        System.out.println("Number of vowels = " + count);
+    }
+}
