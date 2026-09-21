282c0c724b34b44af0644d076b7331a27857bb0c
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-21 23:29:55 +0530

    add print subarry

---
+++ Subarry/Print all Subarry/p1.java
@@ -0,0 +1,24 @@
+class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3};
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i; j < arr.length; j++) {
+
+                System.out.print("[");
+
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k]);
+
+                    if (k < j) {
+                        System.out.print(", ");
+                    }
+                }
+
+                System.out.println("]");
+            }
+        }
+    }
+}
