50cf7939b77692e0913e3631d36fa32f70f8c78c
Sangam Amlapure <sangam.s.amlapure@gmail.com>
2026-09-21 23:33:29 +0530

    count total number of subarry

---
+++ Subarry/Count total subarry/p1.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4};
+
+        int n = arr.length;
+
+        int count = n * (n + 1) / 2;
+
+        System.out.println("Total subarrays = " + count);
+    }
+}
